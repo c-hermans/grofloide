778a45f9bb30021cbeff49b95e3a4736080c6f74
c-hermans <dev@chermans.be>
2022-01-19 19:52:34 +0100

    Added DataStore to store ideas
    with Async Storage

--- IdeaScreen.js
+++ IdeaScreen.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Button, Text } from 'react-native';
+
 import Idea from './Idea.js';
 import Comment from './Comment.js';
+import DataStore from './DataStore.js';
 
 let idea = new Idea("ASP.NET web app");
 let comment = new Comment("De naam wordt TCG Masters");
@@ -9,15 +11,23 @@ let comment2 = new Comment("Draait op een server van AWS Lightsail");
 idea.addComment(comment);
 idea.addComment(comment2);
 
+let ideaStore = new DataStore();
+
 const IdeaScreen = () => {
-    return (
-      <View>
-        <Text>{idea.title}</Text>
-          {idea.comments.map((comment, key) =>
+  return (
+    <View>
+      <Text>{ideaStore.getAllIdeas()[0].title}</Text>
+          {ideaStore.getAllIdeas()[0].comments.map((comment, key) =>
             <Text>    {comment.content}</Text>
           )}
-      </View>
-    );
-  }
-  
-  export default IdeaScreen;
+      <Button title='Add idea'
+        onPress={() => ideaStore.addIdea(idea)}></Button>
+      <Button title='Get all ideas'
+        onPress={() => console.log(ideaStore.getAllIdeas())}></Button>
+        <Button title='Clear ideas'
+        onPress={() => console.log(ideaStore.clear())}></Button>
+    </View>
+  );
+}
+
+export default IdeaScreen;
